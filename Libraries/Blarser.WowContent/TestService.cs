458bb169d6e21c70d4bd6fd9b7ec9d9cc9c24133
Alek Ivankovic <alek.ivankovic@rac.com.au>
2022-05-24 10:49:45 +0800

    Added dummy test

---
+++ Libraries/Blarser.WowContent/TestService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blarser.WowContent
+{
+    public class TestModel
+    {
+        public string Name { get; set; }
+        public bool Flag { get; set; }
+    }
+
+    public class TestService : ITestService
+    {
+        public void DoStuff(TestModel t)
+        {
+            t.Flag = true;
+        }
+    }
+
+    public interface ITestService
+    {
+        void DoStuff(TestModel t);
+    }
+}
---
+++ Tests/Blarser.WowContent.Tests/TempTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blarser.WowContent.Tests;
+
+[TestFixture]
+public class TempTest
+{
+    [Test]
+    public void Things_were_done()
+    {
+        var t = new TestModel();
+
+        var service = new TestService();
+
+        service.DoStuff(t);
+
+        Assert.True(t.Flag);
+    }
+}
